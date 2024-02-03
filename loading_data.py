9f011fd66e18d390043e25a918311cceee8bab2b
Yazdan ZandiyeVakili <yazdanzv.1378@gmail.com>
2024-02-03 14:00:24 +0330

    loading data added

---
+++ loading_data.py
@@ -0,0 +1,12 @@
+import os
+
+
+# File path
+PATH_BIG = 'DATASET/NEW/BIG/PPMI/'
+PATH_SMALL = 'DATASET/NEW/SMALL/PPMI/'
+
+class LoadData:
+    def __init__(self):
+        self.images_big = list()
+        self.images_small = list()
+    def load():
